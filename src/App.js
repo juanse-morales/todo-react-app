83814821b245ebff98a73b105b5f8e901c094b52
Juan Morales <jmoralespuello@gmail.com>
2024-09-27 16:36:25 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div>
+      <div className="header">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>ToDo App</h1>
+    </div>
+  );
+};
+
+export default Header;
