69cc8c60efc574949bb034f33574e5262cd9b37b
Juan Morales <jmoralespuello@gmail.com>
2024-09-30 07:58:03 -0500

    REFACTOR: Rename css class

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import ToDoItems from './components/ToDoItems';
 function App() {
   return (
     <div className='main-container'>
-      <div className="header">
+      <div className="header-container">
         <h1 className='header-title'>ToDo App</h1>
       </div>
       <div className='todo-main-container'>
-        <div className='todo-title'>
-          <h3>Mis tareas</h3>
+        <div className='todo-title-container'>
+          <h3 className='todo-title'>Mis tareas</h3>
         </div>
-        <div className='todo-form'>
+        <div className='todo-form-container'>
           <ToDoForm />
         </div>
-        <div className='todo-items'>
+        <div className='todo-items-container'>
           <ToDoItems />
         </div>
       </div>
