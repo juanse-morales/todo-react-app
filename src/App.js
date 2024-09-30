b26e76a2049b4d0cded0f08e7578ca840f93c0db
Juan Morales <jmoralespuello@gmail.com>
2024-09-30 07:47:16 -0500

    CUSTOM: Design ToDo structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
+import ToDoForm from './components/ToDoForm';
 
 function App() {
   return (
     <div>
       <div className="header">
         <Header />
+      </div>
+      <div className='container-main'>
+        <div className='todo-title'>
+          <h3>Mis tareas</h3>
+        </div>
+        <div className='todo-form'>
+          <ToDoForm />
+        </div>
+        <div className='todo-items'>
+          <ToDoItems />
+        </div>
       </div>
     </div>
   );
