3a85e14fa7452dac310b69485078682085341a35
Juan Morales <jmoralespuello@gmail.com>
2024-09-30 10:02:25 -0500

    CUSTOM: Organize items of ToDo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-
-import './App.css';
-import ToDoForm from './components/ToDoForm';
-import ToDoItems from './components/ToDoItems';
+import "./App.css";
+import ToDoForm from "./components/ToDoForm";
+import ToDoItems from "./components/ToDoItems";
 
 function App() {
   return (
-    <div className='main-container'>
+    <div className="main-container">
       <div className="header-container">
-        <h1 className='header-title'>ToDo App</h1>
+        <h1 className="header-title">ToDo App</h1>
       </div>
-      <div className='todo-main-container'>
-        <div className='todo-title-container'>
-          <h3 className='todo-title'>Mis tareas</h3>
+      <div className="todo-main-container">
+        <div className="todo-title-container">
+          <h3 className="todo-title">Lista de tareas</h3>
         </div>
-        <div className='todo-form-container'>
-          <ToDoForm />
-        </div>
-        <div className='todo-items-container'>
-          <ToDoItems />
-        </div>
+
+        <ToDoForm />
+
+        <ToDoItems />
       </div>
     </div>
   );
