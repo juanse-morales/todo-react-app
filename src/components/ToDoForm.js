b13d912d93c50d1ef938e2d5bf2c234e62cc1d37
Juan Morales <jmoralespuello@gmail.com>
2024-09-30 11:44:17 -0500

    CUSTOM: Styling form

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -3,8 +3,18 @@ import './ToDoForm.css';
 
 const ToDoForm = () => {
   return (
-    <div>
-      
+    <div className='form-main-container'>
+      <form className='task-form'>
+        <input 
+          className='task-name-input'
+          type='text'
+          placeholder='Escribe la tarea'
+          name='task-name'
+        />
+        <button className='task-button'>
+          Agregar Tarea
+        </button>
+      </form>
     </div>
   );
 };
