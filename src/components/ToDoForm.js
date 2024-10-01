0debd5e04efc513b89991a9fb125dae59874382d
Juan Morales <jmoralespuello@gmail.com>
2024-10-01 08:15:03 -0500

    CUSTOM: Add CSS styles and functionality

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,15 +1,39 @@
+import { useState } from 'react';
 import './ToDoForm.css';
+import { v4 as uuid } from 'uuid';
 
+const ToDoForm = (props) => {
+  
+  const [inputText, setInputText] = useState('');
 
-const ToDoForm = () => {
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const item = {
+      id: uuid(),
+      name: inputText,
+      isCompleted: false
+    };
+
+    props.onSubmit(item);
+  };
+  
   return (
     <div className='form-main-container'>
-      <form className='task-form'>
+      <form 
+        className='task-form'
+        onSubmit={handleSubmit}
+      >
         <input 
           className='task-name-input'
           type='text'
           placeholder='Escribe la tarea'
           name='task-name'
+          onChange={handleInputChange}
         />
         <button className='task-button'>
           Agregar Tarea
